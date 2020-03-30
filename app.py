be0842e21919a1f7d9d0a3fba0825e6f9c566ec2
Abenezer Mamo <major.abenezer@gmail.com>
2020-03-29 20:19:40 -0700

    added error handling. finalized song publisheing and taking down. sanitized how files are stored in dist.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 # import flask dependencies
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, url_for, abort
 # import to save filename securely
 from werkzeug.utils import secure_filename
 # import sqlalchemy for user db management
@@ -10,9 +10,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # import csrf protection
 from flask_wtf.csrf import CSRFProtect
 # import login manager
-from flask_login import LoginManager, login_user, login_required, current_user, UserMixin, logout_user
+from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user
 # import image upload library
-from flask_uploads import UploadSet, AUDIO, configure_uploads
+from flask_uploads import UploadSet, configure_uploads
 
 # define app
 app = Flask(__name__)
@@ -20,12 +20,16 @@ app = Flask(__name__)
 app.config.from_object('config')
 # configure login manager
 login_manager = LoginManager()
+# define loggin in route
 login_manager.login_view = 'signin'
 login_manager.init_app(app)
 # configure csrf
 csrf = CSRFProtect(app)
 # initialize db
 db = SQLAlchemy(app)
+# configure uploads / define allowed file types
+audio = UploadSet('audio', ('mp3', 'wav', 'webm'))
+configure_uploads(app, audio)
 
 # define user and song schema
 
@@ -49,151 +53,189 @@ class Song(db.Model):
 # create all schema
 db.create_all()
 
-# configure uploads / define allowed file types
-audio = UploadSet('audio', ('mp3', 'wav', 'webm'))
-configure_uploads(app, audio)
 
 # define user loader for flask login
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    try:
+        return User.query.get(int(user_id))
+    except:
+        abort(500)
 
 # define template routes
 @app.route('/', methods=['GET'])
 def home():
-    songs = Song.query.all()
-    return render_template('home.html', songs=songs)
+    try:
+        # get only published songs
+        songs = Song.query.filter_by(published=True).all()
+        return render_template('home.html', songs=songs)
+    except:
+        abort(404)
 
 # about page route function
 @app.route('/about', methods=['GET'])
 def about():
-    return render_template('about.html')
-#################################################
+    try:
+        return render_template('about.html')
+    except:
+        abort(404)
 
-######### AUTHENTICATION ######################
+# AUTHENTICATION
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
-        # get data from from submitted
-        email = request.form['email']
-        password = request.form['password']
-        # check to see if email exists
-        user = User.query.filter_by(email=email).first()
-        if user:
-            return redirect(url_for('signup'))
-        # create new user
-        new_user = User(email=email, password=generate_password_hash(
-            password, method='sha256'))
-        # add user to db
-        db.session.add(new_user)
-        db.session.commit()
-        db.session.close()
-        # return to login
-        return redirect(url_for('signin'))
-    return render_template('signup.html')
+    try:
+        if request.method == 'POST':
+            # get data from from submitted
+            email = request.form['email']
+            password = request.form['password']
+            # check to see if email exists
+            user = User.query.filter_by(email=email).first()
+            if user:
+                return redirect(url_for('signup'))
+            # create new user
+            new_user = User(email=email, password=generate_password_hash(
+                password, method='sha256'))
+            # add user to db
+            db.session.add(new_user)
+            db.session.commit()
+            db.session.close()
+            # return to login
+            return redirect(url_for('signin'))
+        return render_template('signup.html')
+    except:
+        abort(500)
 
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    if request.method == 'POST':
-        # get data from form
-        email = request.form['email']
-        password = request.form['password']
-        # query by email
-        user = User.query.filter_by(email=email).first()
-        # check credentials
-        if not user or not check_password_hash(user.password, password):
-            return redirect(url_for('signin'))
-        # login user using the manager
-        login_user(user)
-        # redirect to upload page
-        return redirect(url_for('cms'))
-    return render_template('signin.html')
+    try:
+        if request.method == 'POST':
+            # get data from form
+            email = request.form['email']
+            password = request.form['password']
+            # query by email
+            user = User.query.filter_by(email=email).first()
+            # check credentials
+            if not user or not check_password_hash(user.password, password):
+                return redirect(url_for('signin'))
+            # login user using the manager
+            login_user(user)
+            # redirect to upload page
+            return redirect(url_for('cms'))
+        return render_template('signin.html')
+    except:
+        abort(500)
 
 
 @app.route('/signout')
 def signout():
-    logout_user()
-    return redirect(url_for('home'))
-########################################################
+    try:
+        # sign out currently logged unser & redirect
+        logout_user()
+        return redirect(url_for('home'))
+    except:
+        abort(500)
 
-######################### CMS ##########################
+
+# CMS
 # cms page route function
 @app.route('/cms', methods=['GET'])
 @login_required
 def cms():
-    songs = Song.query.order_by(Song.id.desc()).all()
-    return render_template('cms.html', songs=songs)
+    try:
+        # get all songs in the database
+        songs = Song.query.order_by(Song.id.desc()).all()
+        return render_template('cms.html', songs=songs)
+    except:
+        abort(500)
 
 # delete route functio
 @app.route('/delete/<int:id>', methods=['POST'])
 @login_required
 def delete(id):
-    song = Song.query.filter_by(id=id).first()
     try:
-        # remove mp3 and webm files from disk if present
-        os.remove(app.config['BASE_DIR'] + url_for('static',
-                                                   filename='audio/' + song.webm_filename))
-        os.remove(app.config['BASE_DIR'] + url_for('static',
-                                                   filename='audio/' + song.mp3_filename))
+        song = Song.query.filter_by(id=id).first()
+        try:
+            # remove mp3 and webm files from disk if present
+            os.remove(app.config['BASE_DIR'] + url_for('static',
+                                                       filename='audio/' + song.webm_filename))
+            os.remove(app.config['BASE_DIR'] + url_for('static',
+                                                       filename='audio/' + song.mp3_filename))
+        except:
+            abort(500)
+        # add delete from directory using song filename here
+        db.session.delete(song)
+        db.session.commit()
+        db.session.close()
+        return redirect(url_for('cms'))
     except:
-        pass
-    # add delete from directory using song filename here
-    db.session.delete(song)
-    db.session.commit()
-    db.session.close()
-    return redirect(url_for('cms'))
+        abort(500)
 
 
 @app.route('/publish/<int:id>', methods=['POST'])
 @login_required
 def publish(id):
-    # get song
-    song = Song.query.filter_by(id=id).first()
-    # set published to true
-    song.published = True
-    # commit to database
-    db.session.commit()
-    db.session.close()
-    return redirect(url_for('cms'))
+    try:
+        # get song by  id
+        song = Song.query.filter_by(id=id).first()
+        # set published to true
+        song.published = True
+        # commit to database
+        db.session.commit()
+        db.session.close()
+        return redirect(url_for('cms'))
+    except:
+        abort(500)
 
 
 @app.route('/unpublish/<int:id>', methods=['POST'])
 @login_required
 def unpublish(id):
-    # get song
-    song = Song.query.filter_by(id=id).first()
-    # set published to true
-    song.published = False
-    # commit to database
-    db.session.commit()
-    db.session.close()
-    return redirect(url_for('cms'))
+    try:
+        # get song
+        song = Song.query.filter_by(id=id).first()
+        # set published to true
+        song.published = False
+        # commit to database
+        db.session.commit()
+        db.session.close()
+        return redirect(url_for('cms'))
+    except:
+        abort(500)
 
 # this currently uploads only to current directory fix that
 @app.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
-    if request.method == 'POST':
-        # get name from the form
-        name = request.form['name']
-        webm_filename = audio.save(request.files['webm'])
-        mp3_filename = audio.save(request.files['mp3'])
-        # assign url to photo
-        webm_url = audio.url(webm_filename)
-        mp3_url = audio.url(mp3_filename)
-        # create photo object
-        song = Song(name=name, webm_url=webm_url, webm_filename=webm_filename,
-                    mp3_url=mp3_url, mp3_filename=mp3_filename)
-        # insert photo into database
-        db.session.add(song)
-        db.session.commit()
-        db.session.close()
-        return redirect(url_for('cms'))
-    return render_template('upload.html')
-########################################################
+    try:
+        if request.method == 'POST':
+            # get name from the form & use secure_fileiname for
+            # edgy and unifrom file name formatting
+            name = secure_filename(request.form['name'])
+            # get the mp3 & webm file & filenames and sanitize them
+            webm_filename = secure_filename(audio.save(request.files['webm']))
+            mp3_filename = secure_filename(audio.save(request.files['mp3']))
+            # assign url to photo
+            webm_url = audio.url(webm_filename)
+            mp3_url = audio.url(mp3_filename)
+            # create song object
+            song = Song(name=name, webm_url=webm_url, webm_filename=webm_filename,
+                        mp3_url=mp3_url, mp3_filename=mp3_filename)
+            # insert song into database
+            db.session.add(song)
+            db.session.commit()
+            db.session.close()
+            return redirect(url_for('cms'))
+        return render_template('upload.html')
+    except:
+        abort(500)
 
-################ ERROR HANDLING ################
+# ERROR HANDLING
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('400.html'), 400
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
